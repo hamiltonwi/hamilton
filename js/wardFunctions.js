89f5f61371d54cc1a59197d4e36a2606818d2947
LitInfo <37569075+LitInfo@users.noreply.github.com>
2019-04-04 15:17:04 -0400

    Update wardFunctions.js

--- js/wardFunctions.js
+++ js/wardFunctions.js
@@ -1,19 +1,36 @@
-$(document).ready(function() {
+var ctr1=0;
+var ctr2=0;
+var ctr3=0;
+var ctr4=0;
+var ctr5=0;
+var ctr6=0;
+var ctr7=0;
+var ctr8=0;
+var ctr9=0;
+var ctr10=0;
+var ctr11=0;
+var ctr12=0;
+var ctr13=0;
+var ctr14=0;
+var ctr15=0;
 
+$(document).ready(function() {
 $('#graph').append('<img id="graphWard1" src="Age/Ward1.PNG" />');
+$('#graph').prepend($('<img>',{id:'graphWard1Gender',src:'Gender/Ward1.PNG'}))
 
 	$('.carousel').carousel({
 	  interval: 2000
 	})
 });
 
-function showData() {
-	var ctr = ctr + 1;
-
-	if(ctr % 2 == 0){
-		$('#graph').append('<img id="graphWard1" src="Age/Ward1.PNG" />');
+function showData1() {
+	ctr1++;
+	if(ctr1 % 2 == 0){
+		$('#graph').prepend($('<img>',{id:'graphWard1Age',src:'Age/Ward1.PNG'}))
+		$('#graph').prepend($('<img>',{id:'graphWard1Gender',src:'Gender/Ward1.PNG'}))
 	}else{
-		$('#graph').remove('<img id="graphWard1" src="Age/Ward1.PNG" />');
+		$("#graph").empty();
 	}
 
 }
+
