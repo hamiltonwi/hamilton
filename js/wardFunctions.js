1ecb01edb645ba124bf96153327f453849af7a02
LitInfo <37569075+LitInfo@users.noreply.github.com>
2019-04-04 11:10:30 -0400

    Update wardFunctions.js

--- js/wardFunctions.js
+++ js/wardFunctions.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 
+$('#graph').append('<img id="graphWard1" src="Age/Ward1.PNG" />');
+
 	$('.carousel').carousel({
 	  interval: 2000
 	})
+});
+
+function showData() {
+	var ctr = ctr + 1;
+
+	if(ctr % 2 == 0){
+		$('#graph').append('<img id="graphWard1" src="Age/Ward1.PNG" />');
+	}else{
+		$('#graph').remove('<img id="graphWard1" src="Age/Ward1.PNG" />');
+	}
 
 }
