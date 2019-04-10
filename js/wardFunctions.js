d028adda4e1c07ea489a462895bae18fb5b13e92
LitInfo <37569075+LitInfo@users.noreply.github.com>
2019-04-10 14:15:17 -0400

    Update wardFunctions.js

--- js/wardFunctions.js
+++ js/wardFunctions.js
@@ -16,9 +16,13 @@ var ctr15=0;
 
 $(document).ready(function() {
 $('#graph').append('<img id="graphWard1" src="Age/Ward1.PNG" />');
+$("#graph").append(" <caption style='width:500px;'>Specifically to Ward 1 the highest population in age is 20-29 years old.</caption>.");
 $('#graph').append($('<img>',{id:'graphWard1Gender',src:'Gender/Ward1.PNG'}))
+$("#graph").append(" <caption style='width:500px;'>Ward 1 has a higher female to male ratio as well higher average population of females than Hamilton.</caption>.");
 $('#graph').append($('<img>',{id:'graphWard1Job',src:'Job/Ward1.PNG'}))
+$("#graph").append(" <caption style='width:500px;'>Higher average of educational and health jobs.</caption>.");
 $('#graph').append($('<img>',{id:'graphWard1Commute',src:'Commute/Ward1.PNG'}))
+$("#graph").append(" <caption style='width:500px;'>More people enjoy walking, cycling, and using the public bus.</caption>.");
 
 	$('.carousel').carousel({
 	  interval: 2000
@@ -30,9 +34,15 @@ function showData1() {
 	ctr1++;
 	if(ctr1 % 2 == 0){
 		$('#graph').append($('<img>',{id:'graphWard1Age',src:'Age/Ward1.PNG'}))
+		$("#graph").append(" <caption style='width:500px;'>Specifically to Ward 1 the highest population in age is 20-29 years old.</caption>.");
 		$('#graph').append($('<img>',{id:'graphWard1Gender',src:'Gender/Ward1.PNG'}))
+		$("#graph").append(" <caption style='width:500px;'>Ward 1 has a higher female to male ratio as well higher average population of females than Hamilton.</caption>.");
 		$('#graph').append($('<img>',{id:'graphWard1Job',src:'Job/Ward1.PNG'}))
-		$('#graph').apend($('<img>',{id:'graphWard1Commute',src:'Commute/Ward1.PNG'}))
+		$("#graph").append(" <caption style='width:500px;'>Higher average of educational and health jobs.</caption>.");
+		$('#graph').append($('<img>',{id:'graphWard1Commute',src:'Commute/Ward1.PNG'}))
+		$("#graph").append(" <caption style='width:500px;'>More people enjoy walking, cycling, and using the public bus.</caption>.");
+		$('#graph').append($('<img>',{id:'graphWard1Dwelling',src:'Dwelling/Ward1.PNG'}))
+		$("#graph").append(" <caption style='width:500px;'>More people enjoy walking, cycling, and using the public bus.</caption>.");
 	}else{
 		$("#graph").empty();
 	}
